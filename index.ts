f5081c4a4a4ee7c62372a4b1deaf09a2f6f7de00
yaskillin <enissay.95@hotmail.fr>
2022-03-15 09:59:02 +0100

    2 files modified

--- index.ts
+++ index.ts
@@ -1,67 +1,79 @@
-// Import stylesheets
-import './style.css';
-// Write TypeScript code!
-const appDiv: HTMLElement = document.getElementById('app');
-appDiv.innerHTML = `<div>
-    <h1>Shopping List</h1>
-    <input placeholder="please add your item" id="itemInput" type="text"/> <button id="add" >add item</button>
-    <ul id="itemList"></ul>
-</div>`;
-let itemInput = document.getElementById('itemInput') as HTMLInputElement;
-const listItem = document.getElementById('itemList');
-type Grocery = 'Pear' | 'Banana' | 'Ananas';
-interface ShoppingListType {
-  groceries: Grocery[];
-}
 class ShoppingList {
-  groceries: string[] = [];
-
-  addItem(item: string) {
+  constructor() {
+    this.groceries = [];
+  }
+  addItem(item) {
     this.groceries = [...this.groceries, item];
   }
-
-  removeItem(item: string) {
+  removeItem(item) {
     this.groceries = this.groceries.filter((grocery) => grocery !== item);
   }
+  searchInGroceries(item) {
+    return this.groceries.filter((grocery) =>
+      grocery.toLowerCase().includes(item.toLowerCase())
+    );
+  }
+  save() {
+    localStorage.setItem('groceries', JSON.stringify(this.groceries));
+    alert('Groceries saved to database !');
+  }
+  load() {
+    if (localStorage.getItem('groceries')) {
+      this.groceries = JSON.parse(localStorage.getItem('groceries'));
+      alert('Groceries loaded from database !');
+    } else {
+      alert('Empty database !');
+    }
+  }
 }
-
 const myList = new ShoppingList();
-
-const inputDOM = document.getElementById('input') as HTMLInputElement;
+const inputDOM = document.getElementById('input');
+const searchInputDOM = document.getElementById('search');
 const addButton = document.getElementById('add');
 const removeButton = document.getElementById('remove');
 const grociesList = document.getElementById('groceries-list');
-
 addButton.addEventListener('click', () => {
   if (inputDOM.value !== '') {
     myList.addItem(inputDOM.value);
     inputDOM.value = '';
-
     displayList();
     inputDOM.focus();
   } else {
     alert("You can't add an empty item");
   }
 });
-
 removeButton.addEventListener('click', () => {
   if (inputDOM.value !== '') {
     myList.removeItem(inputDOM.value);
     inputDOM.value = '';
-
     displayList();
     inputDOM.focus();
   } else {
     alert("You can't remove an empty item");
   }
 });
-
-const displayList = () => {
+searchInputDOM.addEventListener('keyup', () => {
+  if (searchInputDOM.value !== '') {
+    displayList(myList.searchInGroceries(searchInputDOM.value));
+  } else {
+    displayList();
+  }
+});
+const displayList = (groceries) => {
+  const groceriesLoop = groceries ? groceries : myList.groceries;
   grociesList.innerHTML = '';
-  myList.groceries.forEach((grocery) => {
+  groceriesLoop.forEach((grocery) => {
     let newLi = document.createElement('li');
+    newLi.classList.add('grocery-item');
     newLi.innerHTML = grocery;
-
     grociesList.appendChild(newLi);
   });
 };
+const saveData = () => {
+  myList.save();
+};
+const loadData = () => {
+  myList.load();
+  displayList();
+};
+//# sourceMappingURL=script.js.map
